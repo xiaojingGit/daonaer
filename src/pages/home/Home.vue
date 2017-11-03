224f77dce839f2ca8e5bc94340dae5b4a274b02d
xiaojingGit <1941180150@qq.com>
2017-11-03 16:15:40 +0800

    热销商品列表

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,20 +1,25 @@
 <template>
-  	<div>
+  	<div class="page">
     	<index-header />
     	<index-banner-swiper />
+        <hot-sell-list />
     </div>
 </template>
 <script>
 import header from "./Header.vue"; //局部组件
 import swiper from './BannerSwiper.vue';
+import hotSellList from './HotSellList.vue';
 export default {
   components:{
       "index-header": header,
-      "index-banner-swiper": swiper
+      "index-banner-swiper": swiper,
+      "hot-sell-list": hotSellList
   }
 }
 </script>
 
 <style scoped>
-
+    .page{
+        background: #f5f5f5;
+    }
 </style>
