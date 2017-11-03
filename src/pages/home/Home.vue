959e7fd7b1a57819e9bb9aeb2d901bd7828a5690
chenjiaxuu <249233507@qq.com>
2017-11-03 20:03:38 +0800

    打折

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,15 +2,18 @@
   	<div>
     	<index-header />
     	<index-banner-swiper />
+      <index-activity />
     </div>
 </template>
 <script>
 import header from "./Header.vue"; //局部组件
 import swiper from './BannerSwiper.vue';
+import activity from "./activity.vue"; //获取组件
 export default {
   components:{
       "index-header": header,
-      "index-banner-swiper": swiper
+      "index-banner-swiper": swiper,
+      "index-activity": activity
   }
 }
 </script>
