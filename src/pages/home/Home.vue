641e4fd0e685ca7a17f02ee15429b4832e2e206e
cuiyanjing <cyjing27@163.com>
2017-11-03 20:19:20 +0800

    add icon-swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,16 +2,19 @@
   	<div>
     	<index-header />
     	<index-banner-swiper />
+        <index-icon-swiper />
     </div>
 </template>
 <script>
 import header from "./Header.vue"; //局部组件
 import swiper from './BannerSwiper.vue';
+import iconSwiper from "./IconSwiper.vue";
 export default {
-  components:{
-      "index-header": header,
-      "index-banner-swiper": swiper
-  }
+    components:{
+        "index-header": header,
+        "index-banner-swiper": swiper,
+        "index-icon-swiper": iconSwiper
+    }
 }
 </script>
 
