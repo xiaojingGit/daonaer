514959252497c3871e32c54e724be2ed49a68e96
voggen <137430409@qq.com>
2017-11-10 15:54:35 +0800

    chang-fang-second

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import Vue from 'vue'
 import App from './app/App'
 import router from './router'
-import store from './vuex'
 import FastClick from 'fastclick'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import store from './vuex'
